72d3aaf1a506eb25668302a9b3e115cd3e61a4da
Dom <dguido@udel.edu>
2026-05-10 21:40:05 -0400

    Bug

--- refactored_code/app.py
+++ refactored_code/app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from pathlib import Path
 import time
-
+from dotenv import load_dotenv
+load_dotenv()
 
 from data.store_data import StoreData
 from service.store_service import StoreService

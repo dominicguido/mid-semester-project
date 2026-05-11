af778ff0cc115a3b10321aa9f7540f7f159dfaba
Dom <dguido@udel.edu>
2026-05-11 19:39:18 -0400

    Fixing AI Again

--- refactored_code/service/store_service.py
+++ refactored_code/service/store_service.py
@@ -106,7 +106,7 @@ class StoreService:
         full_messages = [system_prompt] + messages
 
         response = self.client.chat.completions.create(
-            model="gpt-5",
+            model="gpt-4o-mini",
             messages=full_messages,
             temperature=0.3
         )
